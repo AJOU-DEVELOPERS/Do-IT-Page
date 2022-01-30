70b3b9b084ca62a200950965759c781b84bce27d
jin-Pro <dnjun2@ajou.ac.kr>
2022-01-30 22:59:07 +0900

    Feat : FE 기초 환경 셋팅 Styled-component, TS,Recoil,Router

---
+++ FrontEnd/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: pink;
+`;
+
+function App() {
+  return <Container>ttt</Container>;
+}
+
+export default App;
