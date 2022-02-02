c819cf45fa22d121340e5d015e865f1e4b5799ec
jin-Pro <dnjun2@ajou.ac.kr>
2022-02-02 15:41:49 +0900

    Feat : Test 용 데이터 네이밍 수정

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,33 +1,33 @@
-import React from "react";
+import { Suspense } from "react";
+import { Oval } from "react-loader-spinner";
 import { Route, Switch } from "react-router-dom";
-import styled from "styled-components";
+
 import AdminPage from "@Pages/Admin";
 import BoardPage from "@Pages/Board";
 import ErrorPage from "@Pages/Error";
 import LoginPage from "@Pages/Login";
 import MainPage from "@Pages/Main";
 import MyPage from "@Pages/MyPage";
-import PrivateRoute from "Routes/PrivateRoute";
-import AdminRoute from "Routes/AdminRoute";
 
-const Header = styled.div`
-  width: 100px;
-  height: 100px;
-  background-color: pink;
-`;
+import Header from "@Organisms/Header";
+
+import PrivateRoute from "@Route/PrivateRoute";
+import AdminRoute from "@Route/AdminRoute";
 
 const App = () => {
   return (
     <>
-      <Header>hi</Header>
-      <Switch>
-        <Route path="/" component={MainPage} exact />
-        <Route path="/board" component={BoardPage} exact />
-        <Route path="/login" component={LoginPage} exact />
-        <PrivateRoute path="/mypage" component={MyPage} />
-        <AdminRoute path="/admin" component={AdminPage} />
-        <Route path="*" component={ErrorPage} exact />
-      </Switch>
+      <Header />
+      <Suspense fallback={<Oval color="blue" width={100} height={100} />}>
+        <Switch>
+          <Route path="/" component={MainPage} exact />
+          <Route path="/board" component={BoardPage} exact />
+          <Route path="/login" component={LoginPage} exact />
+          <PrivateRoute path="/mypage" component={MyPage} />
+          <AdminRoute path="/admin" component={AdminPage} />
+          <Route path="/error" component={ErrorPage} />
+        </Switch>
+      </Suspense>
     </>
   );
 };
