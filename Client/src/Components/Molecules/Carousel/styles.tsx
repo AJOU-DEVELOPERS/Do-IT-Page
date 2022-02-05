875b48d94dd9c3749724f61c75ffca66b580e0c3
jin-Pro <dnjun2@ajou.ac.kr>
2022-02-05 16:53:49 +0900

    Feat : Carousel 생성

---
+++ Client/src/Components/Molecules/Carousel/styles.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  overflow: hidden;
+`;
+
+const Slider = styled.div`
+  display: flex;
+  display: -webkit-box;
+  width: 100%;
+`;
+
+export { Slider, Container };
