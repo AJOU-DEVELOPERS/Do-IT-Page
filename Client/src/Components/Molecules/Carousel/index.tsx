3a39b2830936f18c16b901700fdee35715bf9a78
jin-Pro <dnjun2@ajou.ac.kr>
2022-02-05 16:53:49 +0900

    Feat : Carousel 생성

---
+++ Client/src/Components/Molecules/Carousel/index.tsx
@@ -0,0 +1,38 @@
+import { useRef } from "react";
+import { Slider, Container } from "./styles";
+
+// carousel 다른 프로젝트에서도 재사용 가능 판단하여 interface를 추출하지 않음.
+interface CarouselProps {
+  children: JSX.Element[];
+  timer: number;
+}
+
+const Carousel = ({ children, timer }: CarouselProps) => {
+  const sliderRef = useRef<HTMLDivElement>(null);
+  let currentSlide = 1;
+
+  setInterval(() => {
+    if (!sliderRef?.current) return;
+
+    sliderRef.current.style.transition = "all 0.5s ease-in-out";
+    sliderRef.current.style.transform = `translateX(-${currentSlide}00%)`; // 백틱을 사용하여 슬라이드로 이동하는 애니메이션을 만듭니다.
+
+    currentSlide = calCurrentNum({
+      total: children.length,
+      prev: currentSlide,
+    });
+  }, timer);
+
+  return (
+    <Container>
+      <Slider ref={sliderRef}>{children}</Slider>
+    </Container>
+  );
+};
+
+const calCurrentNum = ({ total, prev }: { total: number; prev: number }) => {
+  if (total === prev + 1) return 0;
+  return prev + 1;
+};
+
+export default Carousel;
