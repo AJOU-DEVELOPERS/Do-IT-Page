28344775885c6f64a2671725fa4c1e4e8df5411b
jin-Pro <dnjun2@ajou.ac.kr>
2022-02-06 22:39:53 +0900

    Feat : Main Header

---
+++ Client/src/Components/Molecules/Header/index.tsx
@@ -0,0 +1,13 @@
+import Logo from "@Atoms/Logo";
+import { LeftContainer, Title } from "./styles";
+
+const HeaderLeftSide = () => {
+  return (
+    <LeftContainer>
+      <Logo />
+      <Title>Do-IT</Title>
+    </LeftContainer>
+  );
+};
+
+export default HeaderLeftSide;
--- Client/src/Components/Organisms/Header/index.tsx
+++ Client/src/Components/Organisms/Header/index.tsx
@@ -1,7 +1,28 @@
-import Container from "./styles";
+import { useCallback } from "react";
+import { withRouter } from "react-router-dom";
+import { History } from "history";
 
-const Header = () => {
-  return <Container>hi</Container>;
+import { SmallLoginButtonType } from "@Constant/.";
+
+import Button from "@Atoms/Button";
+import Container from "./styles";
+import HeaderLeftSide from "@Molecules/Header";
+
+const Header = ({ history }: { history: History }) => {
+  const handleLoginClick = useCallback(() => {
+    history.push("/login");
+  }, []);
+
+  return (
+    <Container>
+      <HeaderLeftSide />
+      <Button
+        {...SmallLoginButtonType}
+        title="로그인"
+        onClick={handleLoginClick}
+      />
+    </Container>
+  );
 };
 
-export default Header;
+export default withRouter(Header);
