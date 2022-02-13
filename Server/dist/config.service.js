0a2d1762b8853a71832e94c6eeb702bb487df268
Youngil <84563221+tommysgit@users.noreply.github.com>
2022-02-13 14:19:51 +0900

    Delete Server/dist directory
    gitignore

--- Server/dist/config.service.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=config.service.js.map
--- Server/dist/src/common/dto/response-common.dto.d.ts
+++
@@ -1,9 +0,0 @@
-export declare abstract class BaseResponse {
-    isSuccess: string;
-    code: number;
-    message: string;
-}
-export declare abstract class ResultResponse extends BaseResponse {
-    constructor();
-    result: any;
-}
