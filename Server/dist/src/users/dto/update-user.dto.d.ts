70b8d4c6ff885fa4e23010d82d0a48453bb48289
kyi9470812@gmail.com <kyi9470812@gmail.com>
2022-02-13 14:37:15 +0900

    Fix : Untracked Files

--- Server/dist/src/users/dto/update-user.dto.d.ts
+++
@@ -1,5 +0,0 @@
-import { SignupUserDto } from './create-user.dto';
-declare const UpdateUserDto_base: import("@nestjs/mapped-types").MappedType<Partial<SignupUserDto>>;
-export declare class UpdateUserDto extends UpdateUserDto_base {
-}
-export {};
