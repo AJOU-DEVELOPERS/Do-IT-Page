82912c7c730bc229695a260e5cd2cdd84f8e2b36
jin-Pro <dnjun2@ajou.ac.kr>
2022-02-17 01:24:28 +0900

    Fix : Admin Page , Sub Section

---
+++ Client/src/Components/Molecules/Admin/SubSection/Notice/index.tsx
@@ -0,0 +1,5 @@
+const NoticeContainer = () => {
+  return <div>공지사항 관리 탭입니다.</div>;
+};
+
+export default NoticeContainer;
---
+++ Client/src/Components/Molecules/Admin/SubSection/Study/index.tsx
@@ -0,0 +1,5 @@
+const StudyContainer = () => {
+  return <div>스터디 관리 탭입니다.</div>;
+};
+
+export default StudyContainer;
