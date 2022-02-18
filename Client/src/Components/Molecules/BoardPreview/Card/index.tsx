3031f280edf80d17e0b6ee60fdab1972609c5ded
Hansol Hong <66724340+Noelsky-code@users.noreply.github.com>
2022-02-18 19:48:09 +0900

    Feat : 카드 preview 추가

--- Client/src/Components/Molecules/BoardPreview/Card/index.tsx
+++ Client/src/Components/Molecules/BoardPreview/Card/index.tsx
@@ -1,7 +1,36 @@
+import ApplyButton from "@Atoms/Button/Apply";
+import Icon from "@Atoms/Icon";
+import { USER_ICON_URL } from "@Constant/.";
+import { ApplyButtonType } from "@Style/.";
 import { ProjectContentType } from "@Type/.";
+import { Container, Head, Status, Info, Title, InfoContainer } from "./style";
 
-const CardBoardPreview = ({}: ProjectContentType) => {
-  return <div>a</div>;
+const CardBoardPreview = ({
+  name,
+  decription,
+  totalHeadcount,
+  leaderUserIdx,
+  leaderName,
+  status,
+  techStack = [],
+}: ProjectContentType) => {
+  const techStackStr = techStack.join("/");
+  console.log(totalHeadcount);
+  return (
+    <Container>
+      <Head>
+        <Status>{status}</Status>
+        <Info>{techStackStr}</Info>
+      </Head>
+      <Title>{name}</Title>
+      <InfoContainer>
+        <Info>{`이름 : ${leaderName}`}</Info>
+        <Info>{`인원 : ${leaderUserIdx} / ${totalHeadcount}`}</Info>
+        <Icon url={USER_ICON_URL} alt="user icon" />
+      </InfoContainer>
+      <ApplyButton {...ApplyButtonType()} />
+    </Container>
+  );
 };
 
 export default CardBoardPreview;
