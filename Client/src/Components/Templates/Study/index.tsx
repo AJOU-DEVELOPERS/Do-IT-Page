0481e39e562b3ee1e705ac930154162d1725f517
coreight98 <maxcha98@naver.com>
2022-02-20 10:12:50 +0900

    fix : add divide logic 나중에 select로 옮겨야 하는 로직입니다. 각 상태에 따라 분리해서 보여주게 작성했습니다.

--- Client/src/Components/Templates/Study/index.tsx
+++ Client/src/Components/Templates/Study/index.tsx
@@ -1,8 +1,14 @@
 import Header from "@Organisms/Header";
-import Postic from "@Molecules/Postic";
 import Modal from "@Molecules/Modal";
-import { Title, PosticWrapper, Container } from "./styles";
+import StudyContainer from "@Organisms/Study";
+import { Title, ModalContainer, Container } from "./styles";
 import { useCallback, useEffect, useRef, useState } from "react";
+import { GET_STUDY_CONTENT_URL } from "@Constant/index";
+import { hasBoardContent } from "@Util/index";
+import { useRecoilValue } from "recoil";
+import { BoardContentSelector } from "@Recoil/BoardContent";
+import { ContentType } from "@Type/.";
+import BoardPreview from "@Molecules/BoardPreview";
 
 interface Props {
   title: string;
@@ -17,33 +23,53 @@ const StudyTemp = () => {
     console.log("click userIcon");
   }, []);
 
-  useEffect(() => {
-    console.log(joinModalOnOff);
-  }, [joinModalOnOff]);
   const handlePosticClick = (value: Props) => setJoinModalOnOff(value);
+
   const outSection = useRef(null);
+
+  const boardContents =
+    hasBoardContent(GET_STUDY_CONTENT_URL, "스터디") &&
+    useRecoilValue<ContentType[]>(BoardContentSelector(GET_STUDY_CONTENT_URL));
+
+  const { collecting, processing, done } = boardContents?.slice(0, 7).reduce(
+    (result: any, element: any) => {
+      result[element.status].push(element);
+      return result;
+    },
+    { collecting: [], processing: [], done: [] }
+  );
 
   return (
     <>
       {joinModalOnOff && (
         <Modal onClick={(e) => outSection.current !== e.target && setJoinModalOnOff(undefined)}>
-          <Container ref={outSection}>
+          <ModalContainer ref={outSection}>
             <h2>{joinModalOnOff.title}</h2>
             <p>이름 : {joinModalOnOff.studyLeader}</p>
             <p>인원 : 1 / {joinModalOnOff.member}</p>
             <p>정말로 신청하시겠습니까??</p>
-          </Container>
+          </ModalContainer>
         </Modal>
       )}
       <Header onClick={handleUserIconClick} />
-      <Title>스터디 모집</Title>
-      <PosticWrapper>
-        {Array(10)
-          .fill("")
-          .map((element) => (
-            <Postic handlePosticClick={handlePosticClick} />
+      <Title>스터디</Title>
+      <Container>
+        <StudyContainer title={"모집 중"}>
+          {collecting.map((content: ContentType, index: number) => (
+            <BoardPreview key={index} previewType={"card"} content={content} />
           ))}
-      </PosticWrapper>
+        </StudyContainer>
+        <StudyContainer title={"진행 중"}>
+          {processing.map((content: ContentType, index: number) => (
+            <BoardPreview key={index} previewType={"card"} content={content} />
+          ))}
+        </StudyContainer>
+        <StudyContainer title={"마감"}>
+          {done.map((content: ContentType, index: number) => (
+            <BoardPreview key={index} previewType={"card"} content={content} />
+          ))}
+        </StudyContainer>
+      </Container>
     </>
   );
 };
