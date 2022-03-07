cd1c496bb2950c03c5495fb256aa21a74fae181e
Kwondaehwi <kwondh8561@ajou.ac.kr>
2022-03-07 14:41:12 +0900

    Refactor: 스웨거 수정

--- Server/src/reservation/reservation.controller.ts
+++ Server/src/reservation/reservation.controller.ts
@@ -1,10 +1,10 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
 import { ApiBody, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
-import { BaseSuccessResponse, ResultSuccessResponse } from 'src/commons/dto/response-common.dto';
+import { BaseSuccessResponse } from 'src/commons/dto/response-common.dto';
 import { CreateReservationDto } from './dto/create-reservation.dto';
 import { DeleteReservationDto } from './dto/delete-reservation.dto';
+import { GetResesrvationResponseDTO } from './dto/get-reservation.dto';
 import { UpdateReservationDto } from './dto/update-reservation.dto';
-import { Reservation } from './entitiy/reservation.entity';
 import { ReservationService } from './reservation.service';
 
 @Controller('reservation')
@@ -15,7 +15,7 @@ export class ReservationController {
     @Post()
     @ApiOperation({
         summary: '과방 신청 API',
-        description: 'JSON 객체 반환'
+        description: 'true false 반환'
     })
     @ApiBody({ type: CreateReservationDto })
     @ApiOkResponse({ description: '과방 신청 성공', type: BaseSuccessResponse })
@@ -26,45 +26,39 @@ export class ReservationController {
     @Get()
     @ApiOperation({
         summary: '전체 과방 신청 정보 불러오기 API',
-        description: '전체 정보 혹은 빈 배열 반환'
+        description: '전체 정보 혹은 정보 없을 시 빈 배열 반환'
     })
-    @ApiOkResponse({ description: '전체 스터디 불러오기 성공', type: ResultSuccessResponse })
+    @ApiOkResponse({ description: '전체 스터디 불러오기 성공', type: GetResesrvationResponseDTO })
     async findAll(){
-        const reservations = await this.reservationService.findAll()
-        return new ResultSuccessResponse(reservations);
+        return await this.reservationService.findAll()
     }
 
     @Get(':idx')
     @ApiOperation({
         summary: '특정 과방 신청 정보 불러오기 API',
-        description: '성공 시 과방 신청 정보 반환'
+        description: '과방 신청 정보 반환 혹은 정보 없을 시 빈 배열 반환'
     })
-    @ApiOkResponse({ description: '특정 과방 신청 정보 불러오기 성공', type: ResultSuccessResponse })
+    @ApiOkResponse({ description: '특정 과방 신청 정보 불러오기 성공', type: GetResesrvationResponseDTO })
     async findOne(@Param('idx') reservationIdx: number){
-        const reservation = await this.reservationService.findOne(reservationIdx);
-        if(!reservation){
-            return new ResultSuccessResponse([]);
-        }
-        return new ResultSuccessResponse(reservation);
+        return await this.reservationService.findOne(reservationIdx);
     }
 
     @Get(':year/:month')
     @ApiOperation({
         summary: '특정 과방 신청 정보 불러오기 API',
-        description: '성공 시 과방 신청 정보 반환'
+        description: '월별 과방 신청 정보 반환 혹은 정보 없을 시 빈 배열 반환'
     })
-    @ApiOkResponse({ description: '특정 과방 신청 정보 불러오기 성공', type: ResultSuccessResponse })
+    @ApiOkResponse({ description: '특정 과방 신청 정보 불러오기 성공', type: GetResesrvationResponseDTO })
     async findMonth(
         @Param('year') year: string,
         @Param('month') month: string){
-        const reservations = await this.reservationService.findMonth(year, month);
-        return new ResultSuccessResponse(reservations);
+        return await this.reservationService.findMonth(year, month);
     }
 
     @Patch(':idx')
     @ApiOperation({
         summary: '과방 신청 정보 수정 API',
-        description: '성공 시 과방 신청 정보 반환'
+        description: 'true false 반환'
     })
     @ApiBody({ type: UpdateReservationDto })
     @ApiOkResponse({ description: '과방 신청 정보 수정 성공', type: BaseSuccessResponse })
@@ -107,7 +101,7 @@ export class ReservationController {
         summary: '과방 신청 정보 삭제 API',
         description: 'true false 반환'
     })
-    @ApiOkResponse({ description: '과방 신청 정보 삭제 성공' })
+    @ApiOkResponse({ description: '과방 신청 정보 삭제 성공', type: BaseSuccessResponse })
     remove(
         @Param('idx') idx: number,
         @Body() deleteReservationDto: DeleteReservationDto) {
