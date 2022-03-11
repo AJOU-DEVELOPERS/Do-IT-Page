d794bdcdebd118fa880101dbe88f30bb990482f0
jin-Pro <dnjun2@ajou.ac.kr>
2022-03-12 03:46:12 +0900

    Fix : Update Page

---
+++ Client/src/Components/Templates/Update/index.tsx
@@ -0,0 +1,14 @@
+import Header from "@Organisms/Common/Header";
+import UpdateBody from "@Organisms/Update";
+import Footer from "@Organisms/Common/Footer";
+
+const UpdateTemplate = () => {
+  return (
+    <>
+      <Header />
+      <UpdateBody />
+      <Footer />
+    </>
+  );
+};
+export default UpdateTemplate;
