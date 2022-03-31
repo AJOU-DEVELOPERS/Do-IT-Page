9a19cf74272d3342afa66f4e7028b71d9f72dfb6
jin-Pro <dnjun2@ajou.ac.kr>
2022-03-31 13:59:15 +0900

    Fix : API Token

--- Client/src/API/Account/index.ts
+++ Client/src/API/Account/index.ts
@@ -28,20 +28,12 @@ export const postLoginInfo = async (body: LoginInfoType) => {
 };
 
 export const postRegisterInfo = async (body: RegisterInfoType) => {
-  const { data } = await axios.post(
-    POST_REGISTER_INFO,
-    body,
-    GET_HEADER_TOKEN()
-  );
+  const { data } = await axios.post(POST_REGISTER_INFO, body);
   return data;
 };
 
 export const postRequestMail = async (body: { email: string }) => {
-  const { data } = await axios.post(
-    POST_REQUEST_MAIL,
-    body,
-    GET_HEADER_TOKEN()
-  );
+  const { data } = await axios.post(POST_REQUEST_MAIL, body);
   return data;
 };
 
@@ -49,18 +41,14 @@ export const postCheckMail = async (body: {
   cacheKey: string;
   authNum: string;
 }) => {
-  const { data } = await axios.post(POST_CHECK_MAIL, body, GET_HEADER_TOKEN());
+  const { data } = await axios.post(POST_CHECK_MAIL, body);
   return data;
 };
 
 export const checkDuplicateUserId = async ({ id }: { id: string }) => {
-  const { data } = await axios.post(
-    POST_CHECK_DUPLICATE_USER_ID,
-    {
-      id,
-    },
-    GET_HEADER_TOKEN()
-  );
+  const { data } = await axios.post(POST_CHECK_DUPLICATE_USER_ID, {
+    id,
+  });
   return data;
 };
 
